0232190372109ed80cdb268be4ed7210745f5144
hari984422 <hari9844225181@gmail.com>
2025-10-31 13:53:12 +0530

    Final Commit

--- EPharma/src/test/java/com/epharma/testscript/PromotionsModule.java
+++ EPharma/src/test/java/com/epharma/testscript/PromotionsModule.java
@@ -1,11 +1,17 @@
 package com.epharma.testscript;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class PromotionsModule {
 	@Test
 public void doPromotions() {
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.fb.com");
 	Reporter.log("Promotions are being made",true);
+	Reporter.log(driver.getTitle(),true);
+	driver.quit();
 }
 }
